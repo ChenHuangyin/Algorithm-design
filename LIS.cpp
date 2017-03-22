90a5f25103a76b23b371316e5a6802904b15358e
Squarepants <chenhy98@mail2.sysu.edu.cn>
2017-03-22 19:02:48 +0800

    Update LIS.cpp

--- LIS.cpp
+++ LIS.cpp
@@ -6,13 +6,14 @@ int main()
 	int n;
 	cin >> n;
 	int *data=new int [n];
-	int *ans=new int [n];
+	int *ans=new int [n+1];
 	for(int i=0;i<n;i++) 
 	{
 		cin >> data[i];
 		ans[i]=0;
 	}
-	ans[0]=data[0];
+	ans[n]=0;
+	ans[1]=data[0];
 	int len=1;
 	for(int i=1;i<n;i++)  //O(nlogn)
 	{
