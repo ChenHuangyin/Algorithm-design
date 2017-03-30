70c7c7551e7f9407afa2a744113e1b58b352f400
Squarepants <chenhy98@mail2.sysu.edu.cn>
2017-03-30 19:44:41 +0800

    Add files via upload

---
+++ Sicily/lab2-dp/13062_SubDiagonal_Paths.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+long long dp[31][31];
+
+int main()
+{
+	int n;
+	while(cin >> n && n!=0)
+	{
+		memset(dp,0,sizeof(dp));
+		for(int i=0;i<=n;i++)
+		{
+			for(int j=0;j<=n;j++)
+			{
+				
+				
+				if(i==j && j!=0)
+				dp[i][j]=dp[i][j-1];
+				else if(i>j && j!=0)
+				dp[i][j]=dp[i][j-1]+dp[i-1][j];
+				else if(i>j && j==0)
+				dp[i][j]=1;
+				
+				
+			}
+		}
+		cout << dp[n][n] << endl;
+	}	
+}
